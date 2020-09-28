47f8cd90565ff3b251348dd834e0fee97bf90f0b
romaisaev <roma.isaev.001@mail.ru>
2020-09-28 15:24:34 +0300

    Initial Commit

---
+++ Tutors_2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Tutors_2
+//
+//  Created by Рома Исаев on 28.09.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Tutors_2/Tutors_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Tutors_2App.swift
+//  Tutors_2
+//
+//  Created by Рома Исаев on 28.09.2020.
+//
+
+import SwiftUI
+
+@main
+struct Tutors_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
